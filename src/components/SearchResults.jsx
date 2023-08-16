bfcafe06fc56849bff6199f1d941e977a55b72c3
darylbg <darylxcuf@gmail.com>
2023-08-16 21:37:33 +0100

    debugged artist search result display. now artist display as text is searched

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -5,39 +5,46 @@ export default function SearchResults({
   handleSelectedTrack,
   searchSelect,
 }) {
-  if (!searchData || !searchData.tracks || !searchData.tracks.items) {
-    // Return some placeholder content or null if searchData or its properties are not available
-    return null;
+  switch (searchSelect) {
+    case "track":
+      if (!searchData || !searchData.tracks || !searchData.tracks.items) {
+        return null;
+      }
+      return (
+        <ul className="search-results-ul">
+          {searchData.tracks.items.map((item) => (
+            <li key={item.id} data-id={item.id} onClick={handleSelectedTrack}>
+              <p>{item.name}</p>
+              {item.artists.map((artist, index) => (
+                <p key={index}>{artist.name}</p>
+              ))}
+            </li>
+          ))}
+        </ul>
+      );
+    case "artist":
+      if (!searchData || !searchData.artists || !searchData.artists.items) {
+        return null;
+      }
+      return (
+        <ul className="search-results-ul">
+          {searchData.artists.items.map((artist) => (
+            <li key={artist.id}>{artist.name}</li>
+          ))}
+        </ul>
+      );
+    case "genre":
+      if (!searchData || !searchData.genres || !searchData.genres.items) {
+        return null;
+      }
+      return (
+        <ul className="search-results-ul">
+          {searchData.genres.items.map((genre) => (
+            <li key={genre.id}>{genre.name}</li>
+          ))}
+        </ul>
+      );
+    default:
+      return null;
   }
-
-  return (
-    <ul className="search-results-ul">
-      {(() => {
-        switch (searchSelect) {
-          case "track":
-            return searchData.tracks.items.map((item) => (
-              <li key={item.id} data-id={item.id} onClick={handleSelectedTrack}>
-                <p>{item.name}</p>
-                {item.artists.map((artist, index) => (
-                  <p key={index}>{artist.name}</p>
-                ))}
-              </li>
-            ));
-          case "artist":
-            return searchData.artists.items.map((artist) => (
-              <li>
-                {console.log('artist')}
-              </li>
-            ));
-          case "genre":
-            return searchData.genre.items.map((album) => (
-              <li>
-              </li>
-            ));
-          default:
-            return null;
-        }
-      })()}
-    </ul>
-  );
 }
